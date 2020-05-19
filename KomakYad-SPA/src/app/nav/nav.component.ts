f1801dd3764480e4d3aba7d4002b1d01c23c208f
Kiarash Alinasab <aalinasab@outlook.com>
2020-05-18 22:17:41 -0500

    add guards

--- KomakYad-SPA/src/app/nav/nav.component.ts
+++ KomakYad-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,16 +10,18 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(response => {
+    this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Logged in successfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/dashboard']);
     });
   }
 
@@ -29,5 +32,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
--- KomakYad-SPA/src/app/routes.ts
+++ KomakYad-SPA/src/app/routes.ts
@@ -3,11 +3,19 @@ import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './Dashboard/Dashboard.component';
 import { CardManagementComponent } from './CardManagement/CardManagement.component';
 import { CollectionManagementComponent } from './CollectionManagement/CollectionManagement.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'card', component: CardManagementComponent },
-    { path: 'collection', component: CollectionManagementComponent },
-    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+            { path: 'card', component: CardManagementComponent, canActivate: [AuthGuard] },
+            { path: 'collection', component: CollectionManagementComponent},
+        ]
+    },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
