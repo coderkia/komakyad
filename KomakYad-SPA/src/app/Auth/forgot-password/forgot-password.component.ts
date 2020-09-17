282f147aa0c1891f72c2718f2220d58ce0607a44
Kiarash Alinasab <aalinasab@gmail.com>
2020-09-16 22:40:51 -0500

    add form and controls to forgot pass component

--- KomakYad-SPA/src/app/Auth/forgot-password/forgot-password.component.ts
+++ KomakYad-SPA/src/app/Auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { AuthService } from 'src/app/_services/auth.service';
@@ -9,15 +9,24 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-
+  @Output() cancelled = new EventEmitter();
   form: FormGroup;
   loading: boolean;
   reCaptchaToken: string;
   constructor(private authService: AuthService, private alertify: AlertifyService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.createForm()
+    this.createForm();
   }
+
+  forgotPass() {
+
+  }
+
+  cancel() {
+    this.cancelled.emit();
+  }
+
   createForm() {
     this.form = this.formBuilder.group({
       email: ['', Validators.required, Validators.email],
