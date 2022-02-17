18f5dd61b0e826cb83a80f2de2acce2320ebad08
Kiarash Alinasab <aalinasab@outlook.com>
2022-02-16 21:01:14 -0700

    added generic repository

---
+++ Kia.KomakYad.Domain/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Kia.KomakYad.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Kia.KomakYad.Domain.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected DataContext context;
+        public GenericRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public T Add(T entity)
+        {
+            return context.Add(entity).Entity;
+        }
+
+        public async Task<IEnumerable<T>> All()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate)
+        {
+            return await context.Set<T>()
+                .AsQueryable()
+                .Where(predicate)
+                .ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            context.Update(entity);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Kia.KomakYad.Domain/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Kia.KomakYad.Domain.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<IEnumerable<T>> All();
+        Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
+        Task SaveChangesAsync();
+    }
+}
